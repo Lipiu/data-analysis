bf17e4f894ccb7bbaa29d9c59abc9acb27e56612
Rosu Liviu-Mihai <rosuliviu10@gmail.com>
2026-01-02 22:53:00 +0200

    plotted two more charts about battery efficiency while using/not using apps

--- main.py
+++ main.py
@@ -35,13 +35,36 @@ for age, value in zip(ages,values):
         used_labels.add(legend_label)
     plt.bar(age, value, color=table_color, label=legend_label)
 
-plt.plot(ages, p(ages), "--", color='black', label='Trend Line')
-plt.xlabel('Age')
-plt.ylabel('Average screen time (hours/day)')
-plt.title('Average screen time per age')
-plt.legend()
-plt.show()
+# plt.plot(ages, p(ages), "--", color='black', label='Trend Line')
+# plt.xlabel('Age')
+# plt.ylabel('Average screen time (hours/day)')
+# plt.title('Average screen time per age')
+# plt.legend()
+# plt.show()
 
 #---------------------------------------------------------------#
 
+# How fast battery drains for each Phone model (and Operating System)
+# raw battery usage, independent of usage
+battery_by_os_model = (
+    df.groupby(['Device Model', 'Operating System'])['Battery Drain (mAh/day)']
+      .mean()
+      .sort_values(ascending=False)
+)
+# battery_by_os_model.plot(kind='bar', figsize=(10, 5))
+# plt.ylabel('Battery Drain (mAh/day) - Lower is better')
+# plt.title('Average Battery Drain by Device Model and Operating System')
+# plt.xticks(rotation=45, ha='right')
+# plt.tight_layout()
+# plt.show()
 
+
+# compare OS and model battery performance while using apps
+df['Battery drain per minute'] = df['Battery Drain (mAh/day)'] / df['App Usage Time (min/day)']
+drain_by_model_os = df.groupby(['Device Model', 'Operating System'])['Battery drain per minute'].mean()
+# drain_by_model_os.plot(kind='bar', figsize=(12, 6))
+# plt.ylabel('Battery Drain (mAh per minute of app usage)')
+# plt.title('Battery Drain While Using Apps by Device Model and OS')
+# plt.xticks(rotation=45, ha='right')
+# plt.tight_layout()
+# plt.show()
