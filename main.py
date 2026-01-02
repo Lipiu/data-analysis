af40209a88cf2ab50ee49b641b3cb5ef8db41a78
Rosu Liviu-Mihai <rosuliviu10@gmail.com>
2026-01-02 20:17:42 +0200

    added a trend line and plotted a chart

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ screen_time_by_age = df.groupby('Age')['Screen On Time (hours/day)'].mean()
 ages = screen_time_by_age.index
 values = screen_time_by_age.values
 top_values = 3 # number of top values to be displayed
-threshold = np.sort(values)[-top_values]
+top_indices = np.sort(values)[-top_values]
+
+# Trend Line
+trend_line = np.polyfit(ages, values, 1)
+p = np.poly1d(trend_line)
+
 plt.figure(figsize=(10, 5))
 
 used_labels = set()
 
 for age, value in zip(ages,values):
-    if value >= threshold:
+    if value >= top_indices:
         table_color='tab:red'
         legend_label='Top 3 highest values'
     else:
@@ -30,6 +35,7 @@ for age, value in zip(ages,values):
         used_labels.add(legend_label)
     plt.bar(age, value, color=table_color, label=legend_label)
 
+plt.plot(ages, p(ages), "--", color='black', label='Trend Line')
 plt.xlabel('Age')
 plt.ylabel('Average screen time (hours/day)')
 plt.title('Average screen time per age')
